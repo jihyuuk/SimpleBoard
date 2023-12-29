73eb975dda979d55f1e435076d040bfccf7ec8cc
jihyuuk <jihyuk.dev@gmail.com>
2023-12-29 16:46:44 +0900

    #5 Refactor : 레파지토리 리팩토링함

---
+++ src/main/java/com/simple/board/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.simple.board.repository;
+
+import com.simple.board.domain.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
---
+++ src/main/java/com/simple/board/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.simple.board.repository;
+
+import com.simple.board.domain.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post,Long> {
+}
