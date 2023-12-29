5d2e389f435f2620da205a221b005592b826366d
jihyuuk_home <jihyuk.dev@gmail.com>
2023-12-29 22:06:26 +0900

    #5 content관련 서비스,레포지토리 생성

---
+++ src/main/java/com/simple/board/repository/ContentRepository.java
@@ -0,0 +1,9 @@
+package com.simple.board.repository;
+
+import com.simple.board.domain.entity.Content;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContentRepository extends JpaRepository<Content,Long> {
+}
---
+++ src/main/java/com/simple/board/service/ContentService.java
@@ -0,0 +1,18 @@
+package com.simple.board.service;
+
+import com.simple.board.domain.entity.Content;
+import com.simple.board.repository.ContentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ContentService {
+
+    private final ContentRepository contentRepository;
+
+    public void save(Content content){
+        contentRepository.save(content);
+    }
+
+}
