0bd7b9086f2feb5c4710d488db0e574f8f67a2d5
jihyuuk <jihyuk.dev@gmail.com>
2023-12-29 16:45:42 +0900

    #5 테스트 데이터 만들어주는 클래스 추가

---
+++ src/main/java/com/simple/board/domain/entity/InItDatas.java
@@ -0,0 +1,42 @@
+package com.simple.board.domain.entity;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Component
+public class InItDatas {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Transactional
+    public void init(){
+        User user = new User("aaa@gmail.com", "123123", "userA");
+        User userB = new User("bbb@gmail.com", "123123", "userB");
+        User userC = new User("ccc@gmail.com", "123123", "userC");
+        em.persist(user);
+        em.persist(userB);
+        em.persist(userC);
+
+        Category category = new Category("커뮤니티");
+        Category category2 = new Category("Q&A");
+        Category category3 = new Category("익명게시판");
+        em.persist(category);
+        em.persist(category2);
+        em.persist(category3);
+
+        Content content = new Content("모든 국민은 법률이 정하는 바에 의하여 선거권을 가진다. 선거에 관한 경비는 법률이 정하는 경우를 제외하고는 정당 또는 후보자에게 부담시킬 수 없다.");
+        em.persist(content);
+
+        Post post = new Post(category,user,"제목제목제제목",content);
+        em.persist(post);
+
+        Reply reply1 = new Reply(post, userB, "현재 국비과정 뭐시기 저시기");
+        Reply reply2 = new Reply(post,userC,"힘내십쇼!");
+        em.persist(reply1);
+        em.persist(reply2);
+    }
+}
