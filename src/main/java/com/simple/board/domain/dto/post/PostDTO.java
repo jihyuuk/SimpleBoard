f3fc9523d6a4c50edb2764e15a1f5ec6be5186f9
jihyuuk <jihyuk.dev@gmail.com>
2023-12-29 16:48:32 +0900

    #5 Refactor : post의 view 수정함

--- src/main/java/com/simple/board/domain/dto/post/PostDTO.java
+++ src/main/java/com/simple/board/domain/dto/post/PostDTO.java
@@ -1,21 +1,40 @@
-//package com.simple.board.domain.dto.post;
-//
-//import com.simple.board.domain.entity.Post;
-//import lombok.*;
-//
-//@Getter @Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class PostDTO {
-//
-//    Long id;
-//    String title;
-//    String content;
-//
-//
-//    public PostDTO(Post post) {
-//        this.id = post.getId();
-//        this.title = post.getTitle();
-//        this.content = post.getContent();
-//    }
-//}
+package com.simple.board.domain.dto.post;
+
+import com.simple.board.domain.entity.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class PostDTO {
+
+    Long id;
+    String categoryName;
+    String userName;
+    String title;
+    String content;
+    //ReplyDTO로 변경해야함
+    List<Reply> replies = new ArrayList<>();
+
+    int likes;
+    int hates;
+    int views;
+
+    LocalDateTime lastModifiedDate;
+
+    public PostDTO(Post post) {
+        id = post.getId();
+        categoryName = post.getCategory().getName();
+        userName = post.getUser().getName();
+        title = post.getTitle();
+        content = post.getContent().getText();
+        replies =  post.getReplies();
+        likes = post.getLikes();
+        hates = post.getHates();
+        views = post.getViews();
+        lastModifiedDate = post.getLastModifiedDate();
+    }
+}
