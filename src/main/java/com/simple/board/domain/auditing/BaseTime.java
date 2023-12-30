c6ca7c0bb44a3d580a06df5ba76b729d4a90821c
jihyuuk_home <jihyuk.dev@gmail.com>
2023-12-31 05:36:49 +0900

    Refactor: 작성시간을 n전으로 표기하게 바꿈

--- src/main/java/com/simple/board/domain/auditing/BaseTime.java
+++ src/main/java/com/simple/board/domain/auditing/BaseTime.java
@@ -10,6 +10,7 @@ import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 @Getter
 @MappedSuperclass
@@ -26,6 +27,34 @@ public class BaseTime {
     private LocalDateTime deletedDate;
 
     public String getLastModifiedDate(){
-        return lastModifiedDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm"));
+        LocalDateTime now = LocalDateTime.now();
+
+        long years = ChronoUnit.YEARS.between(lastModifiedDate, now);
+        if (years > 0) {
+            return years + "년 전";
+        }
+
+        long months = ChronoUnit.MONTHS.between(lastModifiedDate, now);
+        if (months > 0) {
+            return months + "개월 전";
+        }
+
+        long days = ChronoUnit.DAYS.between(lastModifiedDate, now);
+        if (days > 0) {
+            return days + "일 전";
+        }
+
+        long hours = ChronoUnit.HOURS.between(lastModifiedDate, now);
+        if (hours > 0) {
+            return hours + "시간 전";
+        }
+
+        long minutes = ChronoUnit.MINUTES.between(lastModifiedDate, now);
+        if (minutes > 0) {
+            return minutes + "분 전";
+        }
+
+        return "방금전";
     }
+
 }
