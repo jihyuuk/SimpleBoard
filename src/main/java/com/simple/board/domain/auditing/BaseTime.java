3f3f18fe7f3661b3c94a9e7fc414b6fed6168457
jihyuuk_home <jihyuk.dev@gmail.com>
2024-01-08 07:47:49 +0900

    Modify : BaseTime 클래스 추상클래스로 변경

--- src/main/java/com/simple/board/domain/auditing/BaseTime.java
+++ src/main/java/com/simple/board/domain/auditing/BaseTime.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class BaseTime {
+public abstract class BaseTime {
 
     @Column(updatable = false)
     private LocalDateTime createdDate;
