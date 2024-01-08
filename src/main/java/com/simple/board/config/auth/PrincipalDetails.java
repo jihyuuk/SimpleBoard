d62372f52626e62cd33824feefd4f780f59ad026
jihyuuk_home <jihyuk.dev@gmail.com>
2024-01-09 08:08:28 +0900

    userDetails에 getUser()메소드를 추가 하므로써 불필요한 db조회를 줄임

--- src/main/java/com/simple/board/config/auth/PrincipalDetails.java
+++ src/main/java/com/simple/board/config/auth/PrincipalDetails.java
@@ -56,4 +56,8 @@ public class PrincipalDetails implements UserDetails {
     public boolean isEnabled() {
         return user.isEnabled();
     }
+
+    public User getUser() {
+        return user;
+    }
 }
